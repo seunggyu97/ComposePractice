4ada9097226891777293b8feaa891bc7cd5157a2
seunggyu <seunggyu97@gmail.com>
2023-04-27 20:43:42 +0900

    first Commint

---
+++ component-button/src/main/java/victory/orange/component_button/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package victory.orange.component_button.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
